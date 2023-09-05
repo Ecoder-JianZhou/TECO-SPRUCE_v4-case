b1878cf0fee6b3b766e50cb00b2844e698f17b84
Ecoder-JianZhou <ecoder-zj@outlook.com>
2023-09-05 19:04:40 -0400

    update

---
+++ run_server_all.sh
@@ -0,0 +1,14 @@
+nohup ./run_server_P04_shrub.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P04_shrub.log &
+nohup ./run_server_P04_sphag.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P04_sphag.log &
+nohup ./run_server_P04_tree.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P04_tree.log &
+nohup ./run_server_P04.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P04.log &
+nohup ./run_server_P06.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P06.log &
+nohup ./run_server_P07.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P07.log &
+nohup ./run_server_P08.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P08.log &
+nohup ./run_server_P10.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P10.log &
+nohup ./run_server_P11.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P11.log &
+nohup ./run_server_P13.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P13.log &
+nohup ./run_server_P16.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P16.log &
+nohup ./run_server_P17.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P17.log &
+nohup ./run_server_P19.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P19.log &
+nohup ./run_server_P20.sh > /home/zhou_j/docs_spruce/1_TECO_model/outputs/P20.log &
